423ea8f9257b03df6dd5a0bfe3a7585f6ffc76c8
Marcos <devmarcos7@gmail.com>
2024-10-27 21:26:20 -0300

    criando primeira rota e testando

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
+// importando o fastify
 const fastify = require("fastify");
 
+// passando a funçao para uma constante
 const app = fastify({ logger: true });
 
+
+app.get("/", async (request, reply) => {
+  reply.send({ hello: "world" })
+});
+
+// exportando a constante como modulo
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const app = require('./app')
 
+// inicializando servidor
 const start = async () => {
+
+  // se tudo ocorre bem faça isso
   try {
-    await app.listen({port: 3000})
+    await app.listen({ port: 3000 })
+    app.log.info(`server listening on http://localhost:3000`)
+
+    // se der erro faça isso
   } catch (error) {
     app.log.error(error)
     process.exit(1)
